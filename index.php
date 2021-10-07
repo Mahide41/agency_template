fa41dad667d5db6edf99ebb849c77cda5135a022
Mahide Hasan Anik <armahidetoanik@gmail.com>
2021-10-07 13:42:37 +0400

    Free Consulting corrected

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <?php
-    include ('header.php');
+include('header.php');
 ?>
-    <!-- slider -->
-    <div class="slider">
-        <div class="container">
-            <!-- row -->
-            <div class="row">
-                <div class="col-lg-6">
-                    <img src="assets/frontend/image/office-with-staff-bdex-human-resource-consultancy.png" alt="human resource company" class="img-fluid">
-                </div>
-                <div class="col-lg-6">
-                    <h1><span class="color-green">Human Resource</span> Company in <br> Dubai, UAE</h1>
-                    <div class="btn btn-black">Get Solution</div>
-                </div>
+<!-- slider -->
+<div class="slider">
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="assets/frontend/image/office-with-staff-bdex-human-resource-consultancy.png" alt="human resource company" class="img-fluid">
+            </div>
+            <div class="col-lg-6">
+                <h1><span class="color-green">Human Resource</span> Company in <br> Dubai, UAE</h1>
+                <div class="btn btn-black">Get Solution</div>
             </div>
         </div>
     </div>
-       <!-- free consulting -->
-       <div class="container">
-            <h2> Joint our team to create the best digital business solutions</h2>
-            <p>Interested in trying something different, Ready to get started</p>
-            <button class="con-button">
-            Free Consulting >
-            </button >
-        </div>
+</div>
+<!-- free consulting -->
+<div class="consulting">
+    <div class="container free-consulting">
+        <h2> Joint our team to create the best digital <br> business solutions</h2>
+        <p>Interested in trying something different, Ready to get started</p>
+        <button class="btn btn-black">Free Consulting</button>
+    </div>
+</div>
 <?php
-    include ('footer.php');
+include('footer.php');
 ?>
