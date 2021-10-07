d4290fe90d759bd7e6578a0d306b301e2354a6cb
Mahide Hasan Anik <armahidetoanik@gmail.com>
2021-10-07 13:03:10 +0400

    Navigation completed

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
     include ('header.php');
 ?>
-
-
+    <!-- slider -->
+    <div class="slider">
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <div class="col-lg-6">
+                    <img src="assets/frontend/image/office-with-staff-bdex-human-resource-consultancy.png" alt="human resource company" class="img-fluid">
+                </div>
+                <div class="col-lg-6">
+                    <h1><span class="color-green">Human Resource</span> Company in <br> Dubai, UAE</h1>
+                    <div class="btn btn-black">Get Solution</div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
     include ('footer.php');
 ?>
--- navigation.php
+++ navigation.php
@@ -1,6 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Agency Template</a>
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="https://hr.bdexgroup.com/vendor/images/bdexhrlogo.png.png" alt="bdexhrconsultancies" class="img-fluid" width="150px">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -31,7 +33,7 @@
                     <a class="nav-link" href="#">Signup</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Sigin</a>
+                    <a class="nav-link" href="#">Signin</a>
                 </li>
             </ul>
         </div>
