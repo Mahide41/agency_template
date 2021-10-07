a4d6680d5699c04b7bedcbe64ca0027cb25468bd
Mahide Hasan Anik <armahidetoanik@gmail.com>
2021-10-07 11:54:47 +0400

    Project Initital setup

---
+++ footer.php
@@ -0,0 +1,10 @@
+    
+    <!-- footer content -->
+    <footer>
+        <!-- your code will goes here -->
+    </footer>
+    <!-- scripts -->
+    <script src="assets/frontend/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/frontend/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agency Template</title>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="assets/frontend/css/bootstrap.min.css">
+    <!-- template css -->
+    <link rel="stylesheet" href="assets/frontend/css/theme.css">
+</head>
+<body>
+    <!-- header navigation -->
+    <?php 
+        include ('navigation.php');
+    ?>
