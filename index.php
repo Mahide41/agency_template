86a0bcdef51e3ffb90af3732832f5ed6f819c1bb
neethu08 <rathishnitu@gmail.com>
2021-10-07 13:07:17 +0400

    free consultaing section

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 ?>
 
 
+
+
+    <!-- free consulting -->
+    <div class="container">
+        <h2> Joint our team to create the best digital business solutions</h2>
+        <p>Interested in trying something different, Ready to get started</p>
+        <button class="con-button">
+        Free Consulting >
+        </button >
+    </div>
 <?php
     include ('footer.php');
 ?>
